d93870bb8a9570a0084e4f5e7c259d1b7ef0ac05
rdoconnor <ryan.d.oconnor.14@gmail.com>
2018-07-24 10:47:59 -0700

    Fixing initialization bug and adding traffic light tilemap

--- Assets/ImpassMap.cs
+++ Assets/ImpassMap.cs
@@ -9,25 +9,21 @@ public class ImpassMap : MonoBehaviour {
     private Tilemap ground;
     public RoyT.AStar.Grid grid = new RoyT.AStar.Grid(12, 10, 1);
 
-        // Use this for initialization
-    void Start () {
-        ground = this.GetComponent<Tilemap>();
-        var cb = ground.cellBounds;
-        for (int x = cb.xMin; x < cb.xMax; x++)
-        {
-            for(int y = cb.yMin; y < cb.yMax; y++)
-            {
-                TileBase tile = ground.GetTile(new Vector3Int(x, y, 0));
-                if (tile == null)
-                {
-                    grid.BlockCell(new Position(x- cb.xMin, y - cb.yMin));
-                }
-            }
-        }
+    private bool initialized = false;
+
+    void Start ()
+    {
+        this.Initialize();
     }
 
     public Position[] GetPath(Position start, Position end)
     {
+        // 
+        if (!this.initialized)
+        {
+            this.Initialize();
+        }
+
         var cb = ground.cellBounds;
         Position[] path = grid.GetPath(
             new Position(
@@ -52,4 +48,29 @@ public class ImpassMap : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	}
+
+    private void Initialize()
+    {
+        if (initialized)
+        {
+            return;
+        }
+
+        this.ground = this.GetComponent<Tilemap>();
+        var cb = this.ground.cellBounds;
+
+        for (int x = cb.xMin; x < cb.xMax; x++)
+        {
+            for (int y = cb.yMin; y < cb.yMax; y++)
+            {
+                TileBase tile = ground.GetTile(new Vector3Int(x, y, 0));
+                if (tile == null)
+                {
+                    grid.BlockCell(new Position(x - cb.xMin, y - cb.yMin));
+                }
+            }
+        }
+
+        this.initialized = true;
+    }
 }
