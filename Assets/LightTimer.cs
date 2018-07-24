f22905d1236a01bdea2193c59f37f69c623051c1
jojarvis <41590159+jojarvis@users.noreply.github.com>
2018-07-24 16:19:01 -0700

    Unbreakable Changes

---
+++ Assets/LightTimer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LightTimer : MonoBehaviour
+{
+    public bool isRed;
+    private string redLightTime;
+    Text txt;
+
+
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+
+    }
+
+    void Start()
+    {
+        isRed = true;
+        redLightTime = Time.time.ToString("#");
+        txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (isRed && redLightTime != "0")
+        {
+            redLightTime = Time.timeSinceLevelLoad.ToString("#");
+            txt.text = redLightTime;
+        }
+    }
+}
--- Assets/PlayerScore.cs
+++ Assets/PlayerScore.cs
@@ -18,5 +18,4 @@ public class PlayerScore : MonoBehaviour {
 	void UpdateScore () {
         scoreText = "Score: " + score;
 	}
-    
 }
