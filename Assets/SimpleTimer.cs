570df60659de8e8aeb2769203bca3e002306ceb5
jojarvis <41590159+jojarvis@users.noreply.github.com>
2018-07-24 09:54:34 -0700

    Jake did stuff
    Added SimpleTimer script for game timer
    Added UI Canvas and EventSystem

---
+++ Assets/SimpleTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SimpleTimer : MonoBehaviour
+{
+    public bool isRacing;
+    Text txt;
+
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        isRacing = true;
+        txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (isRacing &&
+            txt != null)
+        {
+            var testHoldValue = Time.time.ToString("#");
+            txt.text = testHoldValue;
+        }
+    }
+}
